e15970bf182585c7c977ebc7258ce7c010de8685
LogicFan <fanyongda2012@gmail.com>
2019-07-24 12:03:19 -0400

    add sections field

--- course/course.go
+++ course/course.go
@@ -8,19 +8,23 @@ const (
 
 // Course a class of a course, contains all section and Notes
 type Course struct {
-	subject string
-	catalog string
-	units   string
-	title   string
-	notes   string
-	// sections map[string]Section
+	subject  string
+	catalog  string
+	units    string
+	title    string
+	notes    string
+	sections map[string]Section
 }
 
 // ToString return a string of the object
 func (course Course) ToString() string {
-	return course.subject +
+	retVal := course.subject +
 		" " + course.catalog +
 		" " + course.title +
 		" \tUnits: " + course.units +
 		" \tNotes: " + course.notes
+	for k, v := range course.sections {
+		retVal = retVal + "\n\t[" + k + "] " + v.ToString()
+	}
+	return retVal
 }
