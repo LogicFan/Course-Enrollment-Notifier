30078041e0481ddcb9cdd277be00fefaa4fc535a
LogicFan <fanyongda2012@gmail.com>
2019-07-24 12:03:59 -0400

    add get method

--- course/section.go
+++ course/section.go
@@ -19,3 +19,28 @@ func (section Section) ToString() string {
 		", erl: " + strconv.FormatInt(int64(section.enrollment), 10) +
 		", inst: " + section.instructor
 }
+
+// GetClass return the class id
+func (section Section) GetClass() int {
+	return section.class
+}
+
+// GetSection return the section
+func (section Section) GetSection() string {
+	return section.section
+}
+
+// GetCapacity return the capacity
+func (section Section) GetCapacity() int {
+	return section.capacity
+}
+
+// GetEnrollment return the enrollment
+func (section Section) GetEnrollment() int {
+	return section.enrollment
+}
+
+// GetInstructor return the instructor name
+func (section Section) GetInstructor() string {
+	return section.instructor
+}
