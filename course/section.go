eaa2a45af107abc4c53ee30820eed7624381dacb
LogicFan <fanyongda2012@gmail.com>
2019-07-24 11:34:28 -0400

    add ToString() method

--- course/section.go
+++ course/section.go
@@ -1,5 +1,7 @@
 package course
 
+import "strconv"
+
 // Section contains information about sections
 type Section struct {
 	class      int
@@ -7,13 +9,13 @@ type Section struct {
 	capacity   int
 	enrollment int
 	instructor string
-	reserves   []Reserve
-	held       string
 }
 
-// Reserve contains information about reserve
-type Reserve struct {
-	condition  string
-	capacity   int
-	enrollment int
+// ToString returns the string of given object
+func (section Section) ToString() string {
+	return strconv.FormatInt(int64(section.class), 10) +
+		", " + section.section +
+		", cap: " + strconv.FormatInt(int64(section.capacity), 10) +
+		", erl: " + strconv.FormatInt(int64(section.enrollment), 10) +
+		", inst: " + section.instructor
 }
